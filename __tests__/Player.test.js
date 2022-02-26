b5cc83aa2d061f0e3e827ac2ad314d3b13b98fb6
LF56 <leah.fox7@gmail.com>
2022-02-26 08:17:27 -0500

    added mock test and passing

---
+++ __tests__/Player.test.js
@@ -0,0 +1,26 @@
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+const Player = require('../lib/Player');
+test('creates a player object', () => {
+    const player = new Player('Dave');
+  
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
+      
+  });
+function Player(name = '') {
+    this.name = name;
+  
+    this.health = Math.floor(Math.random() * 10 + 95);
+    this.strength = Math.floor(Math.random() * 5 + 7);
+    this.agility = Math.floor(Math.random() * 5 + 7);
+  }
+  
+  module.exports = Player; 
